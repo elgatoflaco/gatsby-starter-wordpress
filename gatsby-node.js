48fbe13c9ddbdf40286f0f6484e6bfe948038e49
Adrían Rueda <enmaska@gmail.com>
2019-11-30 16:27:26 +0100

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,74 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+const path = require(`path`)
+const slash = require(`slash`)
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  
+  // query content for WordPress posts
+  const result = await graphql(`
+    query {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            title
+            excerpt
+            slug
+          }
+        }
+      }
+      allWordpressPost {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+  // We want to create a detailed page for each
+  // page node. We'll just use the WordPress Slug for the slug.
+  // The Page ID is prefixed with 'PAGE_'
+  result.data.allWordpressPage.edges.forEach(edge => {
+    // Gatsby uses Redux to manage its internal state.
+    // Plugins and sites can use functions like "createPage"
+    // to interact with Gatsby.
+    createPage({
+      // Each page is required to have a `path` as well
+      // as a template component. The `context` is
+      // optional but is often necessary so the template
+      // can query data specific to each page.
+      path: `/${edge.node.slug}/`,
+      component: slash(pageTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+
+
+
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  result.data.allWordpressPost.edges.forEach(edge => {
+    createPage({
+      // will be the url for the page
+      path: edge.node.slug,
+      // specify the component template of your choice
+      component: slash(postTemplate),
+      // In the ^template's GraphQL query, 'id' will be available
+      // as a GraphQL variable to query for this posts's data.
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
---
+++ src/templates/page.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import { graphql } from "gatsby"
+
+class Page extends Component {
+  render() {
+    const StaticPage = this.props.data.wordpressPage
+
+    return (
+      <>
+        <h1>{StaticPage.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: StaticPage.content }} />
+        {/* <div>{StaticPage.content}</div> */}
+      </>
+    )
+  }
+}
+
+export default Page
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    wordpressPage(id: { eq: $id }) {
+      title
+      content
+    }
+  }
+`
